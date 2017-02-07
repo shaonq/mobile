ea46d1b93c5e3bb311798a3eeb7abc5a44f593e3
shaonq@qq.com <shaonq@qq.com>
2017-02-07 23:24:21 +0800

    Create Navigation.js

---
+++ zujian/Navigation.js
@@ -0,0 +1,36 @@
+
+import React from 'react';
+import {Navigator, View} from 'react-native';
+
+const Navigation = React.createClass({
+    render(){
+        return (
+            <Navigator
+                initialRoute={
+                    {
+                        name: '',
+                        component: this.props.component,
+                        index: 0
+                    }
+                }
+                configureScene={
+                    () => {
+                        return Navigator.SceneConfigs.PushFromRight;
+                    }
+                }
+                renderScene = {
+                    (route,navigator) => {
+                        console.log(route);
+                        let Component = route.component;
+                        return (
+                            <View style={{flex:1,marginTop:20}}>
+                                <Component navigator={navigator} route={route} {...route.passProps}/>
+                            </View>
+                        )
+                    }
+                }
+            />
+        )
+    }
+});
+export default Navigation;
